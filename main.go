f31bef05c1b584072d8b1727c54eaa35d717c2da
SyadoWCS <metaxpuorin@gmail.com>
2021-07-06 02:03:12 +0900

    helloworld

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo"
+)
+
+func main() {
+    // Echoのインスタンス作る
+    e := echo.New()
+
+    // ルーティング
+    e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello World")
+    })
+
+    // サーバー起動
+    e.Start(":8080")
+}
