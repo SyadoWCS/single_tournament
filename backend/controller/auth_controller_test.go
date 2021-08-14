eb45c3fec9a9c4876c8d5c683cbc214b4bb6def5
SyadoWCS <metaxpuorin@gmail.com>
2021-08-15 03:16:04 +0900

    [add]authコントローラのTestLoginとTestUserとTestLogoutのテストコード追加

--- backend/controller/auth_controller_test.go
+++ backend/controller/auth_controller_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// テスト用JSON
 var userRegisterJSON = `{"first_name": "test", "last_name": "user", "email": "testuser@example.com", "password": "password", "password_confirm": "password"}`
 var userLoginJSON = `{"email": "testuser@example.com", "password": "password"}`
 
