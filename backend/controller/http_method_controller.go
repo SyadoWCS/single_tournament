48fe4eb828839961d3cb309209a811c4a542041f
SyadoWCS <metaxpuorin@gmail.com>
2021-08-22 13:27:20 +0900

    [upd]大会エントリーの削除をDELETEメソッドでできるように対応

--- backend/controller/http_method_controller.go
+++ backend/controller/http_method_controller.go
@@ -1,8 +1,6 @@
 package controller
 
 import (
-	"fmt"
-
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,7 +8,6 @@ import (
 func MethodOverride(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		if c.Request().Method == "POST" {
-			fmt.Println(c.Request().PostFormValue("_method"))
 			method := c.Request().PostFormValue("_method")
 			if method == "PUT" || method == "DELETE" {
 				c.Request().Method = method
